1ec7daef6144afc26a16fee1f2d015c3671733f0
Michael Dance <michael.dance@gmail.com>
2019-08-07 14:27:27 -0600

    Updated homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+<<<<<<< HEAD
 import logo from './logo.svg';
+=======
+// import logo from './logo-cropped.png';
+import logo from './logo.png';
+>>>>>>> a066b41... Updated homepage
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +25,27 @@ function App() {
           Learn React
         </a>
       </header>
+=======
+      <header>
+        <img src={logo} className="logo" alt="Primus Paint Solutions" />
+      </header>
+      <section>
+        <h1>
+          Coming Soon
+        </h1>
+        <h2>
+          Email
+        </h2>
+        <a href="mailto:amal@primuspaint.ca">amal@primuspaint.ca</a> 
+        <h2>
+          Contact Number
+        </h2>
+        <a href="tel:7808845848">780-884-5848</a>
+        {/* <p>
+          Please email us at <a href="mailto:amal@primuspaint.ca">amal@primuspaint.ca</a>, or contact us at <a href="tel:7808845848">780-884-5848</a>.
+        </p> */}
+      </section>
+>>>>>>> a066b41... Updated homepage
     </div>
   );
 }
