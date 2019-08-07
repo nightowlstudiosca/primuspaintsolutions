f29839c022dd0e85b54e997eb1d61e0bd3775ed3
Michael Dance <michael.dance@gmail.com>
2019-08-07 15:02:19 -0600

    Updates

---
+++ precache-manifest.2002de81410d56e6d41e7aa3bf758264.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cffa8cf0a1b878cce8fb577860dc5df3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0bd0f6a7d43bab6c1c6",
+    "url": "/static/css/main.b4e05128.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f0bd0f6a7d43bab6c1c6",
+    "url": "/static/js/main.1cbe6e5e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b44201062afdcb2618179b66e4cf64ca",
+    "url": "/static/media/logo.b4420106.png"
+  }
+]);
