6847973a28da622d3a0f1c953c8c6d44c077e72a
Michael Dance <michael.dance@gmail.com>
2019-08-07 14:47:07 -0600

    Add landing page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
-import logo from './logo.svg';
-=======
 // import logo from './logo-cropped.png';
 import logo from './logo.png';
->>>>>>> a066b41... Updated homepage
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
       <header>
         <img src={logo} className="logo" alt="Primus Paint Solutions" />
       </header>
@@ -45,7 +25,6 @@ function App() {
           Please email us at <a href="mailto:amal@primuspaint.ca">amal@primuspaint.ca</a>, or contact us at <a href="tel:7808845848">780-884-5848</a>.
         </p> */}
       </section>
->>>>>>> a066b41... Updated homepage
     </div>
   );
 }
